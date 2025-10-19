ee4b152b36335b907db98dda089c3c3ded7cfd5d
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 18:42:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿interface IService { void Request(); }
+
+class RealService : IService { public void Request() => Console.WriteLine("Real Service"); }
+
+class Proxy : IService
+{
+    private RealService _service;
+    public void Request()
+    {
+        _service ??= new RealService();
+        Console.WriteLine("Proxy control");
+        _service.Request();
+    }
+}
+
+class Program { static void Main() => new Proxy().Request(); }
